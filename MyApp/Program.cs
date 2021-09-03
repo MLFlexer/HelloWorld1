30010364fa0de81b61f28b9b11d1f812e5635a8a
Malthe Larsen <malthemlarsen@gmail.com>
2021-09-03 14:19:47 +0200

    added isLeapYear and Testing

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,12 +2,30 @@
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             //prints hello world
             Console.WriteLine("Hello World!");
+        }
+
+        public bool IsLeapYear(int year) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else if (year % 4 == 0)
+            {
+                return true;
+            } else
+            {
+                
+                return false;
+            }
         }
     }
 }
