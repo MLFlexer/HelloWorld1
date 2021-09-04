1cb34f056a1bc92176f2b1ca5052b3438919a151
Malthe Larsen <malthemlarsen@gmail.com>
2021-09-04 17:12:54 +0200

    added leapyear yay or nay method and tests

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,8 +6,27 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            //prints hello world
-            Console.WriteLine("Hello World!");
+            var program = new Program();
+            program.LeapYearYayOrNay();
+        }
+
+        public void LeapYearYayOrNay() 
+        {
+            Console.WriteLine("Please enter year, to check if it is a leap year:");
+            var userInput = Console.ReadLine();
+            LeapYearYayOrNay(userInput);
+        }
+
+        public void LeapYearYayOrNay(string userInput) {
+
+            var year = int.Parse(userInput);
+            if (IsLeapYear(year))
+            {
+                Console.WriteLine("yay");
+            }
+            else {
+                Console.WriteLine("nay");
+            }
         }
 
         public bool IsLeapYear(int year) {
